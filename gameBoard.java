f9d14d2f86116b7bc8ce55ef01b868171e20dd88
Matt <mjstankevicius@gmail.com>
2023-04-26 18:21:00 -0500

    @gameBoard added setter getter

--- gameBoard.java
+++ gameBoard.java
@@ -5,13 +5,26 @@
 public class gameBoard 
 {
     // declare local/private variables
+    private int arrWidth;
+    private int arrHeight;
     // create setter getter so no one can modify
+    public void setArrWidth(int width)
+    {
+        arrWidth = width;
+    }
+    public void setArrHeight(int height)
+    {
+        arrHeight = height;
+    }
     public gameBoard()
     {
+        // maybe intentially leave blank because this is not expected
+        // maybe throw error to handle for unexpected
         // initial Board call
     }
     public gameBoard(int width, int height)
     {
+        private String[][] array = new String[arrWidth][arrHeight];
         // create array of size width and height
     }
     
