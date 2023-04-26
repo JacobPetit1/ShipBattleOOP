4453853177b652b1270654655157cc7c894f817f
Matt <mjstankevicius@gmail.com>
2023-04-26 18:54:57 -0500

    @gameBoard added more set and get

--- gameBoard.java
+++ gameBoard.java
@@ -28,9 +28,18 @@ public class gameBoard
         array = new int[arrWidth][arrHeight];
     }
 
-    // add methods to place elements at an index
+    // method inserts / sets int value at given index
     public void insertInteger(int xIndex, int yIndex, int element)
-    // add method to search index
+    {
+        array[xIndex][yIndex] = element;
+    }
+    // add method to search index?
+
+    // method looks at what the int value is at the given index
+    public int getElementAtIndex(int xIndex, int yIndex)
+    {
+        return array[xIndex][yIndex];
+    }
 
 
     public String toString()
