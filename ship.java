4187e4adfdadc4099f49329cbfa556973798dcf7
Matt <mjstankevicius@gmail.com>
2023-04-26 20:29:12 -0500

    setters and getters for @ship

--- ship.java
+++ ship.java
@@ -15,8 +15,41 @@ public class ship
      * 
      */
 
-
-    // add setters and getters
+    //variables
+    private int shipIdentifier;
+    private int xHeadIndex, yHeadIndex;
+    private int[] head = new int[2];
+    
+    // getters and setters for the head of the ship
+    // and for the index values, but all are private
+    public void setHeadXIndex(int xIndexValue)
+    {
+        xHeadIndex = xIndexValue;
+        head[0] = xHeadIndex;
+    }
+    public void setHeadYIndex(int yIndexValue)
+    {
+        yHeadIndex = yIndexValue;
+        head[1] = yHeadIndex;
+    }
+    public int getHeadXIndex()
+    {
+        return head[0];
+    }
+    public int getHeadYIndex()
+    {
+        return head[1];
+    }
+    // getter and setter for the shipIdentifier value
+    // shipIdentifier: literally an int value so you can tell the ships apart
+    public void setShipIdentifier(int intShipIdentifier)
+    {
+        shipIdentifier = intShipIdentifier;
+    }
+    public int getShipIdentifier()
+    {
+        return shipIdentifier;
+    }
 
     public ship()
     {
