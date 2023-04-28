989a162d4efd8f968d850a1818d074c52151aae5
Matt <mjstankevicius@gmail.com>
2023-04-28 13:59:36 -0500

    main program created, empty

--- shipBattleText.java
+++ shipBattleText.java
@@ -1,4 +1,19 @@
 public class shipBattleText 
 {
-    
+    /*  This class will start to build the rules of the game
+     *  Starting with initial building the board, probably will 
+     * set define the bounds of the board here
+     *  Initially create the player ship to your liking and I might
+     * create a separate class to handle the AI opponent.
+     *  Use the information here to make sure the game works over text
+     * then we can transfer this to the JFrame.
+     * 
+     * 
+     * 
+     */
+
+
+
+
+
 }
