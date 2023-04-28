7e7a21d3c8ae08bec3a12a69cc546540b3b57e00
Matt <mjstankevicius@gmail.com>
2023-04-28 13:24:17 -0500

    completed @ship

--- ship.java
+++ ship.java
@@ -6,17 +6,13 @@
  // NOTE: this does not fully handle all errors
 public class ship 
 {
-    /* TODO: ship direction
-     *      ship constructor
-     * 
-     * 
-     */
-
     //variables
     private int shipIdentifier;
     private int xHeadIndex, yHeadIndex;
     private int[] head = new int[2];
-    private int shipLength = 3; //  may be subject to change
+    private int shipLength; //  may be subject to change
+    private int[] shipArray;
+    private int direction;
     
     // getters and setters for the head of the ship
     // and for the index values, but all are private
@@ -49,9 +45,52 @@ public class ship
         return shipIdentifier;
     }
 
+    public void setShipLength(int length)
+    {
+        shipLength = length;
+    }
+    public int getShipLength()
+    {
+        return shipLength;
+    }
+
+    // direction: 0-vertical, 1-horizontal
+    public void setShipDirection(int shipDirection)
+    {
+        direction = shipDirection;
+    }
+    public int getShipDirection()
+    {
+        return direction;
+    }
+
+
+    // ship constructors
     public ship()
     {
         // blank ship constructor
+    }
+    public ship(int shipNumber)
+    {
+        shipArray = new int[shipLength];
+
+        // fills array with ship identifier value
+        for(int i = 0; i<shipLength; i++)
+        {
+            shipArray[i] = shipIdentifier;
+        }
+    }
+    public ship(int shipNumber, int shipLength, int shipDirection)
+    {
+        setShipIdentifier(shipNumber);setShipLength(shipLength);
+        setShipDirection(shipDirection);
+
+        shipArray = new int[shipLength];
+
+        for(int i = 0; i<shipLength; i++)
+        {
+            shipArray[i] = shipIdentifier;
+        }
     }
 
     
